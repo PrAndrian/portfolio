594a3f4777fd9da024faf3eea16eaa36d68b18b6
WebGPCC <webdevpcc@gmail.com>
2023-12-07 09:03:16 +0100

    routing error try fixing in production

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           errorElement={<Error404 />}
         />
 
-        <Route path="/*" element={<Error404 />} />
+        <Route path="*" element={<Error404 />} />
       </Route>,
     ),
   );
