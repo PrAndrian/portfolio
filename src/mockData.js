e1ea039c6e4936511a22669f1610a00bcb716291
WebGPCC <webdevpcc@gmail.com>
2023-12-07 15:24:45 +0100

    project 3 added

--- src/mockData.js
+++ src/mockData.js
@@ -1,3 +1,11 @@
+import pharmacie from "./assets/project3/projectPharmacie.jpg" 
+import pharmacie1 from "./assets/project3/projectPharmacie1.jpg" 
+import pharmacie2 from "./assets/project3/projectPharmacie2.jpg" 
+import pharmacie3 from "./assets/project3/projectPharmacie3.jpg" 
+import pharmacie4 from "./assets/project3/projectPharmacie4.jpg" 
+import pharmacie5 from "./assets/project3/projectPharmacie5.jpg" 
+import pharmacie6 from "./assets/project3/projectPharmacie6.jpg" 
+
 export const projects= [
     {
         id: "1",
@@ -67,34 +75,34 @@ export const projects= [
     },
     {
         id: "3",
-        title : "Intellectica Online",
+        title : "Pharmacy's Ecommerce",
         year: "2022",
         columns:1,
-        url:"//images.ctfassets.net/g6o2flkpydd9/5J6stIym1cw05HRocj9DxY/054a0a7d7b57ac6ea0c349bb94b1349f/02_copy.png",
-        description : "My Intellect is an online register, dedicated for the students of Intellect Schools of languages. This app is fully functional and offers students opportunity to follow all the metrics important for each class they attend. Available sections in the app are as follows: attendance and test log, homework log and promotional section. Students of Intellect Schools also do their tests via the app, which is helpful and time-effective. The app is designed for iOS, Android and Huawei devices, double-checked and verified by more than 1500 people so far.",
+        url: pharmacie,
+        description : "As a web developer, my primary objective was to revitalize and optimize the underperforming e-commerce website, enhancing its design, user experience, and SEO to drive increased sales of parapharmaceutical products. As part of my internship at OpenClassrooms spanning over a year, I undertook a pivotal role in the revitalization of an e-commerce website. The objective was to improve design, enhance user experience, and optimize SEO for increased performance. This internship provided invaluable hands-on experience in addressing real-world challenges and applying web development skills in a professional setting.",
         company : "Company lalalala",
         images : [
-            "//images.ctfassets.net/g6o2flkpydd9/1PEgZOenCyroM5f64XPut0/11385bd80909d3b75861bd23cc4ed28e/b1.png",
-            "//images.ctfassets.net/g6o2flkpydd9/itP8RsItq612zILaGVV1Q/97ce8cfe49c1a8cf2a61c60ecc2f5723/b3.png",
-            "//images.ctfassets.net/g6o2flkpydd9/6phQOwa0fIefUuMj9URcT7/3696546adab537787fb34f959624a0c1/b4.png",
-            "//images.ctfassets.net/g6o2flkpydd9/5Eg6zBjXL0Mzsv6GFWrRBS/19da541671f747f1109aef080b91c08f/b5.png",
-            "//images.ctfassets.net/g6o2flkpydd9/123KdaWYJohlKKQ8V67x41/374920bc322536a67123cee17291a7f8/Group_7608.png",
-            "//images.ctfassets.net/g6o2flkpydd9/5ShbZjBxaKgNxbt68GKWz9/6cde9bf7b66f916a88a9ed0d77c3e787/Group_7609.png",
+            pharmacie1,
+            pharmacie2,
+            pharmacie3,
+            pharmacie4,
+            pharmacie5,
+            pharmacie6,
         ],
         steps : [
             { 
                 title : "OBJECTIVES",
-                description : `This app is going to help people organize their studies better. It will have an easy access to a student's notes, activities, tests and a chat with support if needed. Intellect is not just another place where you can learn a foreign language. It's you own personal online classbook to make your daily tasks easier.` 
+                description : `As a web developer, my primary objective was to revitalize and optimize the underperforming e-commerce website, enhancing its design, user experience, and SEO to drive increased sales of parapharmaceutical products.` 
             },
             { 
                 title : "OBSTACLES",
                 description : `
-                There are many apps that try to engage users to learn a language and to make them do a a lesson everyday, but they fail to organize their time and tasks and the user gets lost in translation. Literally. Our task is to make it as friendly and as easy to use with encouragements and personal touch and conversation in order for people to feel motivated to learn something new everyday` 
+                The project presented several challenges, including addressing the existing design limitations, improving user experience, and boosting the website's visibility on search engines. Additionally, ensuring integration of parapharmaceutical references into the website posed a complex task.` 
             },
             { 
-                title : "ONBOARDING",
+                title : "RESULTS",
                 description : `
-                There are many apps that try to engage users to learn a language and to make them do a a lesson everyday, but they fail to organize their time and tasks and the user gets lost in translation. Literally. Our task is to make it as friendly and as easy to use with encouragements and personal touch and conversation in order for people to feel motivated to learn something new everyday` 
+                Transformed an underperforming e-commerce website, achieving a remarkable 249.38% ROI in 2023. This success was driven by a strategic overhaul that included design enhancements, user experience improvements, and SEO optimization. The integration of parapharmaceutical references through a webservice not only expanded the product catalog but also streamlined the customer's online shopping experience. These efforts collectively contributed to increased sales, improved customer satisfaction, and elevated the website's overall performance in the competitive market.` 
             }
         ]
     },
